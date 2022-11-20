05d1abe0ead22789a5ed23294d26dcaf5c5b73ab
nazish-qamar <nazish-qamar@users.noreply.github.com>
2022-11-20 23:32:05 +0100

    adding adaboost

--- ensemble_learning/adaboost.py
+++ ensemble_learning/adaboost.py
@@ -1,19 +1,19 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from helper.load_data import LoadData
 
 
-class BaggingModel:
+class AdaboostModel:
     def __init__(self):
         self.X_train = None
         self.X_test = None
         self.y_train = None
         self.y_test = None
         self.tree = None
-        self.bag = None
+        self.ada = None
 
     def preprocess_data(self, df):
         df.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
@@ -34,41 +34,37 @@ class BaggingModel:
                                                                      random_state=1,
                                                                      stratify=y)
 
-    def train_bagging_model(self):
+    def train_adaboost_model(self):
         self.tree = DecisionTreeClassifier(criterion='entropy',
-                                      max_depth=None,
+                                      max_depth=1,
                                       random_state=1)
 
-        self.bag = BaggingClassifier(base_estimator=self.tree,
-                                n_estimators=500,
-                                max_samples=1.0,
-                                max_features=1.0,
-                                bootstrap=True,
-                                bootstrap_features=False,
-                                n_jobs=1,
-                                random_state=1)
+        self.ada = AdaBoostClassifier(base_estimator=self.tree,
+                         n_estimators=500,
+                         learning_rate=0.1,
+                         random_state=1)
 
 
     def calculate_accuracy(self):
-        self.tree = self.tree.fit(self.X_train, self.y_train)
-        y_train_pred = self.tree.predict(self.X_train)
-        y_test_pred = self.tree.predict(self.X_test)
+        tree = self.tree.fit(self.X_train, self.y_train)
+        y_train_pred = tree.predict(self.X_train)
+        y_test_pred = tree.predict(self.X_test)
 
         tree_train = accuracy_score(self.y_train, y_train_pred)
         tree_test = accuracy_score(self.y_test, y_test_pred)
         print('Decision tree train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
 
-        self.bag = self.bag.fit(self.X_train, self.y_train)
-        y_train_pred = self.bag.predict(self.X_train)
-        y_test_pred = self.bag.predict(self.X_test)
+        ada = self.ada.fit(self.X_train, self.y_train)
+        y_train_pred = ada.predict(self.X_train)
+        y_test_pred = ada.predict(self.X_test)
 
-        bag_train = accuracy_score(self.y_train, y_train_pred)
-        bag_test = accuracy_score(self.y_test, y_test_pred)
-        print('Bagging train/test accuracies %.3f/%.3f' % (bag_train, bag_test))
+        ada_train = accuracy_score(self.y_train, y_train_pred)
+        ada_test = accuracy_score(self.y_test, y_test_pred)
+        print('AdaBoost train/test accuracies %.3f/%.3f' % (ada_train, ada_test))
 
 
 df = LoadData().load_raw_data(dataName="wine")
-model = BaggingModel()
+model = AdaboostModel()
 model.preprocess_data(df)
-model.train_bagging_model()
+model.train_adaboost_model()
 model.calculate_accuracy()
