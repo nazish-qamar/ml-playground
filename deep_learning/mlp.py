21ed62105bc307d3e6794365ef0fb8be5f396697
nazish-qamar <nazish-qamar@users.noreply.github.com>
2022-12-14 00:06:35 +0100

    Multilayer perceptron with mnist data

---
+++ deep_learning/mlp.py
@@ -0,0 +1,183 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+from helper.load_mnist import LoadMnist
+
+
+class NeuralNetMLP(object):
+    def __init__(self, n_hidden=30,
+                 l2=0., epochs=100, eta=0.001,
+                 shuffle=True, minibatch_size=1, seed=None):
+
+        self.random = np.random.RandomState(seed)
+        self.n_hidden = n_hidden
+        self.l2 = l2
+        self.epochs = epochs
+        self.eta = eta
+        self.shuffle = shuffle
+        self.minibatch_size = minibatch_size
+
+    def _onehot(self, y, n_classes):
+        onehot = np.zeros((n_classes, y.shape[0]))
+        for idx, val in enumerate(y.astype(int)):
+            onehot[val, idx] = 1.
+        return onehot.T
+
+    def _sigmoid(self, z):
+        """Compute logistic function (sigmoid)"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def _forward(self, X):
+        # step 1: net input of hidden layer
+        z_h = np.dot(X, self.w_h) + self.b_h
+
+        # step 2: activation of hidden layer
+        a_h = self._sigmoid(z_h)
+
+        # step 3: net input of output layer
+        z_out = np.dot(a_h, self.w_out) + self.b_out
+
+        # step 4: activation output layer
+        a_out = self._sigmoid(z_out)
+
+        return z_h, a_h, z_out, a_out
+
+    def _compute_cost(self, y_enc, output):
+        L2_term = (self.l2 *
+                   (np.sum(self.w_h ** 2.) +
+                    np.sum(self.w_out ** 2.)))
+
+        term1 = -y_enc * (np.log(output))
+        term2 = (1. - y_enc) * np.log(1. - output)
+        cost = np.sum(term1 - term2) + L2_term
+
+        return cost
+
+    def predict(self, X):
+        z_h, a_h, z_out, a_out = self._forward(X)
+        y_pred = np.argmax(z_out, axis=1)
+        return y_pred
+
+    def fit(self, X_train, y_train, X_valid, y_valid):
+        n_output = np.unique(y_train).shape[0]  # number of class labels
+        n_features = X_train.shape[1]
+
+        ########################
+        # Weight initialization
+        ########################
+
+        # weights for input -> hidden
+        self.b_h = np.zeros(self.n_hidden)
+        self.w_h = self.random.normal(loc=0.0, scale=0.1,
+                                      size=(n_features, self.n_hidden))
+
+        # weights for hidden -> output
+        self.b_out = np.zeros(n_output)
+        self.w_out = self.random.normal(loc=0.0, scale=0.1,
+                                        size=(self.n_hidden, n_output))
+
+        epoch_strlen = len(str(self.epochs))  # for progress formatting
+        self.eval_ = {'cost': [], 'train_acc': [], 'valid_acc': []}
+
+        y_train_enc = self._onehot(y_train, n_output)
+
+        # iterate over training epochs
+        for i in range(self.epochs):
+
+            # iterate over minibatches
+            indices = np.arange(X_train.shape[0])
+
+            if self.shuffle:
+                self.random.shuffle(indices)
+
+            for start_idx in range(0, indices.shape[0] - self.minibatch_size +
+                                      1, self.minibatch_size):
+                batch_idx = indices[start_idx:start_idx + self.minibatch_size]
+                print(batch_idx)
+                # forward propagation
+                z_h, a_h, z_out, a_out = self._forward(X_train.iloc[batch_idx,])
+
+                ##################
+                # Backpropagation
+                ##################
+
+                # [n_examples, n_classlabels]
+                delta_out = a_out - y_train_enc[batch_idx]
+                # [n_examples, n_hidden]
+                sigmoid_derivative_h = a_h * (1. - a_h)
+
+                # [n_examples, n_classlabels] dot [n_classlabels, n_hidden]
+                # -> [n_examples, n_hidden]
+                delta_h = (np.dot(delta_out, self.w_out.T) *
+                           sigmoid_derivative_h)
+
+                # [n_features, n_examples] dot [n_examples, n_hidden]
+                # -> [n_features, n_hidden]
+                grad_w_h = np.dot(X_train.iloc[batch_idx,].T, delta_h)
+                grad_b_h = np.sum(delta_h, axis=0)
+
+                # [n_hidden, n_examples] dot [n_examples, n_classlabels]
+                # -> [n_hidden, n_classlabels]
+                grad_w_out = np.dot(a_h.T, delta_out)
+                grad_b_out = np.sum(delta_out, axis=0)
+
+                # Regularization and weight updates
+                delta_w_h = (grad_w_h + self.l2 * self.w_h)
+                delta_b_h = grad_b_h  # bias is not regularized
+                self.w_h -= self.eta * delta_w_h
+                self.b_h -= self.eta * delta_b_h
+
+                delta_w_out = (grad_w_out + self.l2 * self.w_out)
+                delta_b_out = grad_b_out  # bias is not regularized
+                self.w_out -= self.eta * delta_w_out
+                self.b_out -= self.eta * delta_b_out
+
+            z_h, a_h, z_out, a_out = self._forward(X_train)
+
+            cost = self._compute_cost(y_enc=y_train_enc,
+                                      output=a_out)
+
+            y_train_pred = self.predict(X_train)
+            y_valid_pred = self.predict(X_valid)
+
+            train_acc = ((np.sum(y_train == y_train_pred)).astype(np.float) /
+                         X_train.shape[0])
+            valid_acc = ((np.sum(y_valid == y_valid_pred)).astype(np.float) /
+                         X_valid.shape[0])
+
+            sys.stderr.write('\r%0*d/%d | Cost: %.2f '
+                             '| Train/Valid Acc.: %.2f%%/%.2f%% ' %
+                             (epoch_strlen, i + 1, self.epochs, cost,
+                              train_acc * 100, valid_acc * 100))
+            sys.stderr.flush()
+
+            self.eval_['cost'].append(cost)
+            self.eval_['train_acc'].append(train_acc)
+            self.eval_['valid_acc'].append(valid_acc)
+        return self
+
+
+X_train, X_test, y_train, y_test = LoadMnist().load_train_test()
+
+n_epochs = 100
+
+
+nn = NeuralNetMLP(n_hidden=100,
+                  l2=0.01,
+                  epochs=n_epochs,
+                  eta=0.0005,
+                  minibatch_size=100,
+                  shuffle=True,
+                  seed=1)
+
+nn.fit(X_train=X_train[:55000],
+       y_train=y_train[:55000],
+       X_valid=X_train[55000:],
+       y_valid=y_train[55000:])
+
+
+plt.plot(range(nn.epochs), nn.eval_['cost'])
+plt.ylabel('Cost')
+plt.xlabel('Epochs')
+plt.show()
---
+++ helper/load_mnist.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+
+
+class LoadMnist:
+    def __init__(self):
+        self.X_train = None
+        self.y_train = None
+        self.y_test = None
+        self.X_test = None
+
+    def load_train_test(self):
+        X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+        y = y.astype(int)
+        X = ((X / 255.) - .5) * 2
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y,
+                                                                                test_size=10000,
+                                                                                random_state=123,
+                                                                                stratify=y)
+
+        return self.X_train, self.X_test, self.y_train, self.y_test
+
+    def visualize(self):
+        fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True)
+        ax = ax.flatten()
+        for i in range(10):
+            img = self.X_train.iloc[[i],].to_numpy().reshape(28, 28)
+            ax[i].imshow(img, cmap='Greys')
+
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+        plt.tight_layout()
+        plt.show()
+
+    def save_compressed_data(self):
+        np.savez_compressed('mnist_scaled.npz',
+                            X_train=self.X_train,
+                            y_train=self.y_train,
+                            X_test=self.X_test,
+                            y_test=self.y_test)
+
+    def load_uncompressed_data(self):
+        mnist = np.load('mnist_scaled.npz')
+        #print(mnist.files)
+        X_train, y_train, X_test, y_test = [mnist[f] for f in ['X_train',
+                                                               'y_train',
+                                                               'X_test',
+                                                               'y_test']]
+
+        del mnist
+        return X_train, y_train, X_test, y_test
+
+
+model_ = LoadMnist()
+X_train, X_test, y_train, y_test = model_.load_train_test()
+model_.save_compressed_data()
